6b1e04479ee68574b316b30e3d7d271a5fc3c325
육이 <145183548+yugi-62@users.noreply.github.com>
2025-09-16 20:54:38 +0900

    Add files via upload

---
+++ 250916.py
@@ -0,0 +1,9 @@
+print("안녕하세요!", end=" ")
+print("반갑습니다.")
+name = input("이름을 입력하세요 : ")
+print("안녕하세요 " + name + "님!")
+
+
+print("ㅎㅇ", end=" ")
+age = int(input("나이 입력하세요 : "))
+print("반갑습니다. 10년 뒤에 %d 살이시군요!" % (age + 10))
